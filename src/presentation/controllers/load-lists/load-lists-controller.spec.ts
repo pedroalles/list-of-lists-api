6637243ac1f08384a7eafd28e55763febca3abda
pedroalles <pedroalles@gmail.com>
2022-04-16 20:09:48 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/load-lists/load-lists-controller.spec.ts
+++ src/presentation/controllers/load-lists/load-lists-controller.spec.ts
@@ -2,17 +2,33 @@ import { IList } from '@/domain/models/list'
 import { ILoadLists } from '@/domain/usecases/load-lists-usecase'
 import { LoadListsController } from './load-lists-controller'
 
-class LoadListsUseCaseStub implements ILoadLists {
-  async load(): Promise<IList[]> {
-    return null
+const makeLoadListsUseCaseStub = (): ILoadLists => {
+  class LoadListsUseCaseStub implements ILoadLists {
+    async load(): Promise<IList[]> {
+      return null
+    }
+  }
+  return new LoadListsUseCaseStub()
+}
+
+type SutTypes = {
+  sut: LoadListsController
+  loadListsUseCaseStub: ILoadLists
+}
+
+const makeSut = (): SutTypes => {
+  const loadListsUseCaseStub = makeLoadListsUseCaseStub()
+  const sut = new LoadListsController(loadListsUseCaseStub)
+  return {
+    sut,
+    loadListsUseCaseStub
   }
 }
 
 describe('LoadLists Controller', () => {
   it('should call LoadListsUseCase load method', async () => {
-    const loadListsUseCaseStub = new LoadListsUseCaseStub()
+    const { sut, loadListsUseCaseStub } = makeSut()
     const loadSpy = jest.spyOn(loadListsUseCaseStub, 'load')
-    const sut = new LoadListsController(loadListsUseCaseStub)
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
   })
