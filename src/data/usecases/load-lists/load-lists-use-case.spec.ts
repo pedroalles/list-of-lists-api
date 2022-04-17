ff10ac58fc0e26199e3989bfe02636ff9c119d5d
pedroalles <pedroalles@gmail.com>
2022-04-16 23:29:21 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/load-lists/load-lists-use-case.spec.ts
+++ src/data/usecases/load-lists/load-lists-use-case.spec.ts
@@ -2,16 +2,32 @@ import { ILoadListsRepository } from '@/data/interfaces/db/load-lists-repository
 import { IList } from '@/domain/models/list'
 import { LoadListsUseCase } from './load-lists-use-case'
 
-class LoadListsRepositoryStub implements ILoadListsRepository {
-  loadAll(): Promise<IList[]> {
-    return null
+const makeLoadListsRepositoryStub = (): ILoadListsRepository => {
+  class LoadListsRepositoryStub implements ILoadListsRepository {
+    loadAll(): Promise<IList[]> {
+      return null
+    }
+  }
+  return new LoadListsRepositoryStub()
+}
+
+type SutTypes = {
+  sut: LoadListsUseCase
+  loadListsRepositoryStub: ILoadListsRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadListsRepositoryStub = makeLoadListsRepositoryStub()
+  const sut = new LoadListsUseCase(loadListsRepositoryStub)
+  return {
+    sut,
+    loadListsRepositoryStub
   }
 }
 
 describe('LoadLists UseCase', () => {
   it('should call LoadListsRepository loadAll method', async () => {
-    const loadListsRepositoryStub = new LoadListsRepositoryStub()
-    const sut = new LoadListsUseCase(loadListsRepositoryStub)
+    const { sut, loadListsRepositoryStub } = makeSut()
     const loadAllSpy = jest.spyOn(loadListsRepositoryStub, 'loadAll')
     await sut.load()
     expect(loadAllSpy).toHaveBeenCalled()
