6e9aae7e7b05145ae1f3fd32db56a5ba39927d12
pedroalles <pedroalles@gmail.com>
2022-04-17 17:19:41 -0300

    refactor: remove ListsInMemoryRepository getter and setter methods

--- src/infra/db/in-memory/lists-in-memory-repository.ts
+++ src/infra/db/in-memory/lists-in-memory-repository.ts
@@ -2,17 +2,9 @@ import { ILoadListsRepository } from '@/data/interfaces/db/load-lists-repository
 import { IList } from '@/domain/models/list'
 
 export class ListsInMemoryRepository implements ILoadListsRepository {
-  protected _lists: IList[] = []
+  public lists: IList[] = []
 
   async loadAll(): Promise<IList[]> {
-    return this._lists
-  }
-
-  public get lists(): IList[] {
-    return this._lists
-  }
-
-  public set lists(data: IList[]) {
-    this._lists = data
+    return this.lists
   }
 }
