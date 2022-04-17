98045d3c9b169cc7be6efb49ea6ddbe139ac0451
pedroalles <pedroalles@gmail.com>
2022-04-17 18:15:42 -0300

    feat: add express app

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,9 @@
-console.log('Yeah!')
+import { Request, Response } from 'express'
+import app from './config/app'
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendStatus(200)
+})
+
+const PORT = 3333
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`))
