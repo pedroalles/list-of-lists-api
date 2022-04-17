913ec79d14da824d825bacdce0cc690c1b55b1eb
pedroalles <pedroalles@gmail.com>
2022-04-16 22:00:54 -0300

    chore: exclude index files from jest coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   moduleNameMapper: {
