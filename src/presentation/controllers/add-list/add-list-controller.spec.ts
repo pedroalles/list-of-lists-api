142befcc59a271547998589b73e4e3eff772ecb7
pedroalles <pedroalles@gmail.com>
2022-04-18 21:09:59 -0300

    refactor: move fake http request creation to a factory method

--- src/presentation/controllers/add-list/add-list-controller.spec.ts
+++ src/presentation/controllers/add-list/add-list-controller.spec.ts
@@ -2,6 +2,15 @@ import { badRequest } from '@/presentation/helpers/http-response'
 import { IHttpRequest, IValidation } from '@/presentation/interfaces'
 import { AddListController } from './add-list-controller'
 
+const makeFakeRequest = (): IHttpRequest => {
+  return {
+    body: {
+      title: 'any_title',
+      description: 'any_description'
+    }
+  }
+}
+
 const makeValidatorStub = (): IValidation => {
   class ValidatorStub implements IValidation {
     validate(input: any): Error {
@@ -29,11 +38,7 @@ describe('AddList Controller', () => {
   it('should call Validation with correct values', async () => {
     const { sut, validatorStub } = makeSut()
     const validateSpy = jest.spyOn(validatorStub, 'validate')
-
-    const httpRequest: IHttpRequest = {
-      body: {}
-    }
-
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
@@ -41,9 +46,7 @@ describe('AddList Controller', () => {
   it('should return 400 if Validator returns an error', async () => {
     const { sut, validatorStub } = makeSut()
     jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(new Error())
-    const httpRequest: IHttpRequest = {
-      body: {}
-    }
+    const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest.body)
     expect(httpResponse).toEqual(badRequest(new Error()))
   })
