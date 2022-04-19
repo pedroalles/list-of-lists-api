d8d6ee45c313a0b0f21632319894607924974102
pedroalles <pedroalles@gmail.com>
2022-04-19 00:54:23 -0300

    test: add AddList composition

--- src/main/routes/lists-routes.test.ts
+++ src/main/routes/lists-routes.test.ts
@@ -1,8 +1,20 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('Lists Routes', () => {
+describe('GET Lists Route', () => {
   it('should return all lists on success', async () => {
     await request(app).get('/api/lists').expect(200)
+  })
+})
+
+describe('POST Lists Route', () => {
+  it('should return all lists on success', async () => {
+    const httpResponse = await request(app)
+      .post('/api/lists')
+      .send({ title: 'any_title', description: 'any_description' })
+      .expect(200)
+
+    expect(typeof httpResponse.body).toBe('string')
+    console.log(httpResponse.body)
   })
 })
