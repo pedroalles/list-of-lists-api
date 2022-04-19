29138822871fa8d1d7f094ed2351c26d4d119ed4
pedroalles <pedroalles@gmail.com>
2022-04-18 23:28:30 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-list/add-list-use-case.spec.ts
+++ src/data/usecases/add-list/add-list-use-case.spec.ts
@@ -9,16 +9,32 @@ const makeFakeListData = (): AddListModel => {
   }
 }
 
-class AddListRepositoryStub implements IAddListRepository {
-  add(data: AddListModel): Promise<string> {
-    return null
+const makeAddListRepositoryStub = (): IAddListRepository => {
+  class AddListRepositoryStub implements IAddListRepository {
+    add(data: AddListModel): Promise<string> {
+      return null
+    }
+  }
+  return new AddListRepositoryStub()
+}
+
+type SutTypes = {
+  sut: AddListUseCase
+  addListRepositoryStub: IAddListRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addListRepositoryStub = makeAddListRepositoryStub()
+  const sut = new AddListUseCase(addListRepositoryStub)
+  return {
+    sut,
+    addListRepositoryStub
   }
 }
 
 describe('AddList UseCase', () => {
   it('should call AddListRepository add method', async () => {
-    const addListRepositoryStub = new AddListRepositoryStub()
-    const sut = new AddListUseCase(addListRepositoryStub)
+    const { sut, addListRepositoryStub } = makeSut()
     const loadAllSpy = jest.spyOn(addListRepositoryStub, 'add')
     await sut.add(makeFakeListData())
     expect(loadAllSpy).toHaveBeenCalledWith(makeFakeListData())
