19002404d9202b4b553e847554498a41208e7b4c
pedroalles <pedroalles@gmail.com>
2022-04-18 23:57:14 -0300

    feat: ensure ListInMemoryRepository add a new list on add success

--- src/infra/db/in-memory/lists-in-memory-repository.ts
+++ src/infra/db/in-memory/lists-in-memory-repository.ts
@@ -1,8 +1,27 @@
+import { IAddListRepository } from '@/data/interfaces/db/add-list-repository'
 import { ILoadListsRepository } from '@/data/interfaces/db/load-lists-repository'
 import { IList } from '@/domain/models/list'
+import { AddListModel } from '@/domain/usecases/add-list-usecase'
 
-export class ListsInMemoryRepository implements ILoadListsRepository {
+export class ListsInMemoryRepository
+  implements ILoadListsRepository, IAddListRepository
+{
   public lists: IList[] = []
+
+  async add(data: AddListModel): Promise<string> {
+    const id = 'random_id'
+
+    this.lists.push({
+      id,
+      title: data.title,
+      description: data.description,
+      items: [],
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+
+    return id
+  }
 
   async loadAll(): Promise<IList[]> {
     return this.lists
