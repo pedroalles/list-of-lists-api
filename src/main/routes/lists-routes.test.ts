c6c6c41efdfb24ce8b0aabdd2e971774507293a9
pedroalles <pedroalles@gmail.com>
2022-04-19 01:04:44 -0300

    test: add AddListUseCase badRequest integration test

--- src/main/routes/lists-routes.test.ts
+++ src/main/routes/lists-routes.test.ts
@@ -8,13 +8,21 @@ describe('GET Lists Route', () => {
 })
 
 describe('POST Lists Route', () => {
-  it('should return all lists on success', async () => {
+  it('should return an id on success', async () => {
     const httpResponse = await request(app)
       .post('/api/lists')
       .send({ title: 'any_title', description: 'any_description' })
       .expect(200)
 
     expect(typeof httpResponse.body).toBe('string')
-    console.log(httpResponse.body)
+  })
+
+  it('should return 400 if invalid body is provided', async () => {
+    const httpResponse = await request(app)
+      .post('/api/lists')
+      .send({ description: 'any_description' })
+      .expect(400)
+
+    expect(httpResponse.body).toEqual({ error: 'Missing field: title' })
   })
 })
