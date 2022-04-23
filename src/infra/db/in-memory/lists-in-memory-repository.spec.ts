b26043e660fcecd2985e8b4017891dbb12487702
pedroalles <pedroalles@gmail.com>
2022-04-22 22:24:58 -0300

    test: ensure ListInMemoryRepository find a list by id on success

--- src/infra/db/in-memory/lists-in-memory-repository.spec.ts
+++ src/infra/db/in-memory/lists-in-memory-repository.spec.ts
@@ -1,4 +1,4 @@
-import { makeFakeLists } from '@/presentation/tests/lists-mock'
+import { makeFakeList, makeFakeLists } from '@/presentation/tests/lists-mock'
 import { ListsInMemoryRepository } from './lists-in-memory-repository'
 
 describe('loadAll ListsInMemory Repository', () => {
@@ -24,5 +24,15 @@ describe('add ListsInMemory Repository', () => {
     const listId = await sut.add(fakeList)
     expect(sut.lists).toHaveLength(1)
     expect(typeof listId).toBe('string')
+  })
+})
+
+describe('loadById ListsInMemory Repository', () => {
+  it('should load a list on success', async () => {
+    const sut = new ListsInMemoryRepository()
+    const fakeList = makeFakeList()
+    sut.lists.push(fakeList)
+    const list = await sut.loadById('any_id')
+    expect(list).toEqual(fakeList)
   })
 })
