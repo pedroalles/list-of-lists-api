975732c084253f30d98883d294f9f5b5987f9701
pedroalles <pedroalles@gmail.com>
2022-04-22 22:20:52 -0300

    feat: ensure ListInMemoryRepository find a list by id on success

--- src/infra/db/in-memory/lists-in-memory-repository.ts
+++ src/infra/db/in-memory/lists-in-memory-repository.ts
@@ -1,11 +1,12 @@
 import { IAddListRepository } from '@/data/interfaces/db/add-list-repository'
+import { ILoadListByIdRepository } from '@/data/interfaces/db/load-list-by-id-repository'
 import { ILoadListsRepository } from '@/data/interfaces/db/load-lists-repository'
 import { IList } from '@/domain/models/list'
 import { AddListModel } from '@/domain/usecases/add-list-usecase'
 import { randomUUID } from 'crypto'
 
 export class ListsInMemoryRepository
-  implements ILoadListsRepository, IAddListRepository
+  implements ILoadListsRepository, IAddListRepository, ILoadListByIdRepository
 {
   public lists: IList[] = []
 
@@ -26,5 +27,9 @@ export class ListsInMemoryRepository
 
   async loadAll(): Promise<IList[]> {
     return this.lists
+  }
+
+  async loadById(id: string): Promise<IList> {
+    return this.lists.find((list) => id === list.id)
   }
 }
