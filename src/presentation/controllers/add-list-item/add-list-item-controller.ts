31a3a3287e56193e8f03f85ce3f558e869293d33
pedroalles <pedroalles@gmail.com>
2022-04-23 22:11:29 -0300

    feat: ensure AddListItemController throws if AddListItemUseCase throws

--- src/presentation/controllers/add-list-item/add-list-item-controller.ts
+++ src/presentation/controllers/add-list-item/add-list-item-controller.ts
@@ -1,7 +1,11 @@
 import { IAddListItem } from '@/domain/usecases/add-list-item-usecase'
 import { ILoadListById } from '@/domain/usecases/load-list-by-id-usecase'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
-import { badRequest, forbidden } from '@/presentation/helpers/http-response'
+import {
+  badRequest,
+  forbidden,
+  serverError
+} from '@/presentation/helpers/http-response'
 import {
   IController,
   IHttpRequest,
@@ -17,10 +21,14 @@ export class AddListItemController implements IController {
   ) {}
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const list = await this.loadListById.load(httpRequest.params.id)
-    if (!list) return forbidden(new InvalidParamError('ListId'))
-    const error = this.validator.validate(httpRequest.body)
-    if (error) return badRequest(error)
-    await this.addListItem.add(httpRequest.body)
+    try {
+      const list = await this.loadListById.load(httpRequest.params.id)
+      if (!list) return forbidden(new InvalidParamError('ListId'))
+      const error = this.validator.validate(httpRequest.body)
+      if (error) return badRequest(error)
+      await this.addListItem.add(httpRequest.body)
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
