fe315fe5b66673a7cc171694a1f1ba235821ae31
pedroalles <pedroalles@gmail.com>
2022-04-23 22:05:20 -0300

    feat: ensure AddListItemController calls Validator with correct values

--- src/presentation/controllers/add-list-item/add-list-item-controller.ts
+++ src/presentation/controllers/add-list-item/add-list-item-controller.ts
@@ -5,18 +5,21 @@ import { forbidden } from '@/presentation/helpers/http-response'
 import {
   IController,
   IHttpRequest,
-  IHttpResponse
+  IHttpResponse,
+  IValidation
 } from '@/presentation/interfaces'
 
 export class AddListItemController implements IController {
   constructor(
     private readonly loadListById: ILoadListById,
-    private readonly addListItem: IAddListItem
+    private readonly addListItem: IAddListItem,
+    private readonly validator: IValidation
   ) {}
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     const list = await this.loadListById.load(httpRequest.params.id)
     if (!list) return forbidden(new InvalidParamError('ListId'))
+    this.validator.validate(httpRequest.body)
     await this.addListItem.add(httpRequest.body)
   }
 }
